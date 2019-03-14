b0babe23e47760aedc5ce870c41d36b558f74584
Vincent <vincent.vincent.davis@gmail.com>
2019-03-14 10:02:55 -0400

    just in case

--- Tip-Calculator/tip-calculator.js
+++ Tip-Calculator/tip-calculator.js
@@ -1,17 +1,27 @@
 function calculateTip() {
         let price = $("#price").val()
+      
         let numberOfPeople = $("#numberOfPeople").val()
         if($("#radio_1").prop("checked", true)){
             tipPercent = .15
+            console.log(tipPercent)
         }
         if($("#radio_2").prop("checked", true)){
             tipPercent = .20
+            console.log(tipPercent)
+
         }
         if($("#radio_3").prop("checked", true)){
             tipPercent = .25
+            console.log(tipPercent)
+
         }
 
         let tipAmount = (price / numberOfPeople) * tipPercent
+        console.log("price", price)
+        console.log("numberOfPeople", numberOfPeople)
+        console.log("tipPercent", tipPercent)
+        console.log("radio_1",$("#radio_1").prop("checked", true) )
     $("#tip").val(tipAmount) 
     console.log(tipAmount)
        
